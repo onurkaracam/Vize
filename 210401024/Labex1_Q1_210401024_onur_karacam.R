59cf5a400aded330ae4709ec7836dfd2214d1269
Onur Karaçam <onur.karacam006@gmail.com>
2023-11-17 02:19:49 +0300

    1.2, testleri ile tamamlandı.

--- 210401024/Labex1_Q1_210401024_onur_karacam.R
+++ 210401024/Labex1_Q1_210401024_onur_karacam.R
@@ -1,4 +1,4 @@
 
 
-maps = read.csv("https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv", sep =";")
+maps <- read.csv("https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv", sep =";")
 
---
+++ 210401024/Labex1_Q1_tests_210401024_onur_karacam.R
@@ -0,0 +1,15 @@
+library(testthat)
+
+setwd("~/GitHub/Vize/210401024")
+
+source("Labex1_Q1_210401024_onur_karacam.R")
+
+#2.4
+test_that("MapsThatChangedOurWorld_StoryMap_Data.csv dosyas?? aktif dizinde mevcut mudur", {
+  expect_true(file.exists("MapsThatChangedOurWorld_StoryMap_Data.csv"))
+})
+
+#2.5
+test_that("'maps' adli degisken Global Workspace'te mevcut mudur", {
+  expect_true(exists("maps"))
+})
